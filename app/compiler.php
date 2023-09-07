1f481ce96a13adc689cf719d9fcf30ba468ac5e4
Udesh <it21013928@my.sliit.lk>
2023-09-07 17:40:41 +0530

    compiler ready

--- app/compiler.php
+++ app/compiler.php
@@ -3,7 +3,7 @@ $language = strtolower($_POST['language']);
 $code = $_POST['code'];
 
 $random = substr(md5(mt_rand()), 0, 7);
-$filePath = "D:\\xampp\htdocs\RookieScript\app\\temp\\" . $random . "." . $language;
+$filePath = "D:\\xampp\htdocs\RookieScriptCompiler\app\\temp\\" . $random . "." . $language;
 $programFile = fopen($filePath, "w");
 fwrite($programFile, $code);
 fclose($programFile);
@@ -17,24 +17,48 @@ if ($language == "php") {
     }
     echo $filePath, " ", $output;
 }
-if ($language == "python") {
-    $output = shell_exec("C:\Users\KOUSIK\AppData\Local\Programs\Python\Python39\python.exe $filePath 2>&1");
+if ($language == "py") {
+    $output = shell_exec("python $filePath 2>&1");
     echo $output;
 }
-if ($language == "node") {
-    rename($filePath, $filePath . ".js");
-    $output = shell_exec("node $filePath.js 2>&1");
-    echo $output;
-}
-if ($language == "c") {
-    $outputExe = $random . ".exe";
-    shell_exec("gcc $filePath -o $outputExe");
-    $output = shell_exec(__DIR__ . "//$outputExe");
-    echo $output;
-}
-if ($language == "cpp") {
-    $outputExe = $random . ".exe";
-    shell_exec("g++ $filePath -o $outputExe");
-    $output = shell_exec(__DIR__ . "//$outputExe");
-    echo $output;
+if ($language == "java") {
+    $files = glob("D:\\xampp\htdocs\RookieScriptCompiler\app\\temp\\" . '/*');
+
+    // Loop through the files and delete them
+    foreach ($files as $file) {
+        // Check if the file is a regular file (not a directory)
+        if (is_file($file)) {
+            unlink($file); // Delete the file
+        }
+    }
+    sleep(1);
+    $random = substr(md5(mt_rand()), 0, 7);
+    $filePath = "D:\\xampp\htdocs\RookieScriptCompiler\app\\temp\\" . $random . "." . $language;
+    $programFile = fopen($filePath, "w");
+    fwrite($programFile, $code);
+    fclose($programFile);
+
+    shell_exec("javac $filePath 2>&1");
+
+    // Get a list of files in the directory
+    $files = glob("D:\\xampp\htdocs\RookieScriptCompiler\app\\temp\\" . '/*');
+
+    // Sort the files by their creation time in descending order
+    usort($files, function ($a, $b) {
+        return filectime($b) <=> filectime($a);
+    });
+
+    // Get the name of the last created file
+    if (!empty($files)) {
+        $lastCreatedFile = $files[0];
+        $pathInfo = pathinfo($lastCreatedFile);
+        $lastCreatedFileName = $pathInfo['filename']; // This will give you the file name without extension
+        // $lastCreatedFileName = basename($lastCreatedFile);
+        // echo "The last created file is: $lastCreatedFileName";
+        $command = 'cd "D:\\xampp\\htdocs\\RookieScriptCompiler\\app\\temp\\" && java ' . $lastCreatedFileName . ' 2>&1';
+        $output = shell_exec($command);
+        echo $output;
+    } else {
+        echo "No files found";
+    }
 }
--- ui/js/ide.js
+++ ui/js/ide.js
@@ -3,24 +3,22 @@ let editor;
 window.onload = function() {
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/c_cpp");
+    editor.session.setMode("ace/mode/java");
 }
 
 function changeLanguage() {
 
     let language = $("#languages").val();
 
-    if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
-    else if(language == 'php')editor.session.setMode("ace/mode/php");
-    else if(language == 'python')editor.session.setMode("ace/mode/python");
-    else if(language == 'node')editor.session.setMode("ace/mode/javascript");
+    if(language == 'py')editor.session.setMode("ace/mode/python");
+    else if(language == 'java')editor.session.setMode("ace/mode/java");
 }
 
 function executeCode() {
 
     $.ajax({
 
-        url: "/RookieScript/app/compiler.php",
+        url: "/RookieScriptCompiler/app/compiler.php",
 
         method: "POST",
 
